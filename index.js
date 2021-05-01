ac9095c86d4123608a2016a78beb20a201f6012f
Dev Vora <52440769+DevVora626@users.noreply.github.com>
2021-05-01 19:13:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+module.exports.meterToCentimeter = (meter) => meter * 100;
+module.exports.meterToKilometer = (meter) => meter * 0.001;
+module.exports.meterToMillimeter = (meter) => meter * 1000;
+module.exports.meterToMicrometer = (meter) => meter * 1000000;
+module.exports.meterToNanometer = (meter) => meter * 1000000000;
+module.exports.meterToFoot = (meter) => meter * 3.280839895;
+module.exports.meterToInch = (meter) => meter * 39.37007874;
+
+module.exports.kilometerToMeter = (kilometer) => kilometer * 1000;
+module.exports.kilometerToCentimeter = (kilometer) => kilometer * 100000;
+module.exports.kilometerToMillimeter = (kilometer) => kilometer * 1000000;
+module.exports.kilometerToMicrometer = (kilometer) => kilometer * 1000000000;
+module.exports.kilometerToNanometer = (kilometer) => kilometer * 1000000000000;
+module.exports.kilometerToFoot = (kilometer) => kilometer * 3280.839895;
+module.exports.kilometerToInch = (kilometer) => kilometer * 39370.07874;
+
+module.exports.centimeterToMeter = (centimeter) => centimeter * 0.01;
+module.exports.centimeterToKilometer = (centimeter) => centimeter * 0.00001;
+module.exports.centimeterToMillimeter = (centimeter) => centimeter * 10;
+module.exports.centimeterToMicrometer = (centimeter) => centimeter * 10000;
+module.exports.centimeterToNanometer = (centimeter) => centimeter * 10000000;
+module.exports.centimeterToFoot = (centimeter) => centimeter * 0.032808399;
+module.exports.centimeterToInch = (centimeter) => centimeter * 0.3937007874;
+
+module.exports.millimeterToMeter = (millimeter) => millimeter * 0.001;
+module.exports.millimeterToKilometer = (millimeter) => millimeter * 0.000001;
+module.exports.millimeterToCentimeter = (millimeter) => millimeter * 0.1;
+module.exports.millimeterToMicrometer = (millimeter) => millimeter * 1000;
+module.exports.millimeterToNanometer = (millimeter) => millimeter * 1000000;
+module.exports.millimeterToFoot = (millimeter) => millimeter * 0.0032808399;
+module.exports.millimeterToInch = (millimeter) => millimeter * 0.0393700787;
+
+module.exports.micrometerToMeter = (micrometer) => micrometer * 0.001;
+module.exports.micrometerToKilometer = (micrometer) => micrometer * 0.000001;
+module.exports.micrometerToCentimeter = (micrometer) => micrometer * 0.1;
+module.exports.micrometerToMillimeter = (micrometer) => micrometer * 1000;
+module.exports.micrometerToNanometer = (micrometer) => micrometer * 1000000;
+module.exports.micrometerToFoot = (micrometer) => micrometer * 0.0032808399;
+module.exports.micrometerToInch = (micrometer) => micrometer * 0.0393700787;
+
+module.exports.nanometerToMeter = (nanometer) => nanometer / 1000000000;
+module.exports.nanometerToKilometer = (nanometer) => nanometer / 1000000000000;
+module.exports.nanometerToCentimeter = (nanometer) => nanometer / 10000000;
+module.exports.nanometerToMillimeter = (nanometer) => nanometer * 0.000001;
+module.exports.nanometerToMicrometer = (nanometer) => nanometer * 0.001;
+
+module.exports.footToMeter = (foot) => foot * 0.3048;
+module.exports.footToKilometer = (foot) => foot * 0.0003048;
+module.exports.footToCentimeter = (foot) => foot * 30.48;
+module.exports.footToMillimeter = (foot) => foot * 304.8;
+module.exports.footToMicrometer = (foot) => foot * 304800;
+module.exports.footToInch = (foot) => foot * 12;
+
+module.exports.inchToMeter = (inch) => inch * 0.0254;
+module.exports.inchToKilometer = (inch) => inch * 0.0000254;
+module.exports.inchToCentimeter = (inch) => inch * 2.54;
+module.exports.inchToMillimeter = (inch) => inch * 25.4;
+module.exports.inchToMicrometer = (inch) => inch * 25400;
+module.exports.inchToFoot = (inch) => inch * 0.0833333333;
