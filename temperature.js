32e75dd9d7f69369700473fa63ba37d5ccc35821
Dev Vora <52440769+DevVora626@users.noreply.github.com>
2021-05-20 15:11:47 +0530

    temperature.js added

---
+++ temperature.js
@@ -0,0 +1,10 @@
+// celsius
+module.exports.celsiusToKelvin = (celsius) => celsius * 274.15;
+module.exports.celsiusToFahrenheit = (celsius) => celsius * 33.8;
+
+// kelvin
+module.exports.kelvinToCelsius = (kelvin) => kelvin * -272.15;
+module.exports.kelvinToFahrenheit = (kelvin) => kelvin * -457.87;
+// fahrenheit
+module.exports.fahrenheitToCelsius = (fahrenheit) => fahrenheit * -17.222222222;
+module.exports.fahrenheitToKelvin = (fahrenheit) => fahrenheit * 255.92777778;
